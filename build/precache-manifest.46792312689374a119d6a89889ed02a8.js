63588a70923d0f02b4d8044b00172f056f5f6bbd
wangruobing <1400121920@qq.com>
2020-10-04 08:04:42 +0800

    commit task1

---
+++ build/precache-manifest.46792312689374a119d6a89889ed02a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8ef645a2b358b9c3469c6c65dfab1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3bda673ef0465014b0c",
+    "url": "/static/js/2.c62f3779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c62f3779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daffb82c2fc0b197aa56",
+    "url": "/static/js/main.e5648df2.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
